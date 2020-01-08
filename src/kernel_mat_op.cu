2b768d2ad7b3be4936747af5eef9a7cbe7e53f78
Henri Giroussens <henri.giroussens@epita.fr>
2020-01-08 20:14:33 +0100

    adding matrix factorization

--- src/kernel_mat_op.cu
+++ src/kernel_mat_op.cu
@@ -3,11 +3,13 @@
 #include "cuda_runtime.h"
 #include <cstdlib>
 #include "kernel_mat_op.hh"
+#include "device_launch_parameters.h"
+
 
 __global__ void matrixAdditionKernel(float* A, float* B, float* C, int N) {
 
     int ROW = blockIdx.y * blockDim.y + threadIdx.y;
-    int COL = blockIdx.x*blockDim.x+threadIdx.x;
+    int COL = blockIdx.x * blockDim.x + threadIdx.x;
 
     float tmpSum = 0;
 
@@ -18,11 +20,49 @@ __global__ void matrixAdditionKernel(float* A, float* B, float* C, int N) {
     C[ROW * N + COL] = tmpSum;
 }
 
+
+__global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) {
+
+    int ROW = blockIdx.y*blockDim.y+threadIdx.y;
+    int COL = blockIdx.x*blockDim.x+threadIdx.x;
+
+    float tmpSum = 0;
+
+    if (ROW < N && COL < N) {
+        // each thread computes one element of the block sub-matrix
+        for (int i = 0; i < N; i++) {
+            tmpSum += A[ROW * N + i] * B[i * N + COL];
+        }
+    }
+    C[ROW * N + COL] = tmpSum;
+}
+
+
+
 void matrixAddition(float *A, float *B, float *C, int N) {
     dim3 threadsPerBlock(N, N);
     dim3 blocksPerGrid(1, 1);
+    if (N*N > 512) {
+        threadsPerBlock.x = 512;
+        threadsPerBlock.y = 512;
+        blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
+        blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
+    }
     matrixAdditionKernel<<<blocksPerGrid,threadsPerBlock>>>(A, B, C, N);
 
 }
+
+void matrixMultiplication(float *A, float *B, float *C, int N){
+    dim3 threadsPerBlock(N, N);
+    dim3 blocksPerGrid(1, 1);
+    if (N*N > 512) {
+        threadsPerBlock.x = 512;
+        threadsPerBlock.y = 512;
+        blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
+        blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
+    }
+    matrixMultiplicationKernel<<<blocksPerGrid,threadsPerBlock>>>(A, B, C, N);
+}
+
 
 #include "kernel_mat_op.hh"
--- src/mat_op.cc
+++ src/mat_op.cc
@@ -6,10 +6,10 @@
 #include <cassert>
 #include <cuda_runtime.h>
 #include "kernel_mat_op.hh"
+#include <iomanip>
 
 
-
-int mat_add(float** A, float** B, int N)
+int mat_add(std::vector<std::vector<float>> A, std::vector<std::vector<float>> B, int N)
 {
     cudaError_t rc = cudaSuccess;
 
@@ -35,19 +35,187 @@ int mat_add(float** A, float** B, int N)
     float* d_B;
     float* d_C;
 
-    rc = cudaMalloc(&d_A, SIZE * sizeof(float));
+    cudaMalloc(&d_A, SIZE * sizeof(float));
     cudaMalloc(&d_B, SIZE * sizeof(float));
     cudaMalloc(&d_C, SIZE * sizeof(float));
-    if (rc)
-        return -1;
 
     // Copy to device
-    cudaMemcpy(&d_A, &h_A, SIZE * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(&d_B, &h_B, SIZE * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemset(&d_C, 0, SIZE * sizeof(float));
+    rc = cudaMemcpy(d_A, &h_A[0], SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    if (rc)
+        std::cout << "error memcpy\n";
+    cudaMemcpy(d_B, &h_B[0], SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemset(d_C, 0, SIZE * sizeof(float));
 
+    // call the kernel
     matrixAddition(d_A, d_B, d_C, N);
+    cudaDeviceSynchronize();
 
+    // copy memory back to host
+    cudaMemcpy(&h_C[0], d_C, SIZE * sizeof(float), cudaMemcpyDeviceToHost);
+
+    //un-flatten matrix
+    std::vector<std::vector<float>> C(N);
+
+    for (int i=0; i<N; i++) {
+        std::vector<float> c(N);
+        C[i] = c;
+        for (int j = 0; j < N; j++) {
+            C[i][j] = h_C[i * N + j];
+        }
+    }
+
+    // compute CPU add
+    std::vector<std::vector<float>> C_true(N);
+
+    for (int i=0; i<N; i++) {
+        std::vector<float> c_true(N);
+        C_true[i] = c_true;
+        for (int j = 0; j < N; j++) {
+            C_true[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    bool same = true;
+    // check true
+    for (int i=0; i<N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (C[i][j] != C_true[i][j])
+                same = false;
+            std::cout << C[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << '\n';
+
+    for (int i=0; i<N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << C_true[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    if (same)
+        std::cout << "succes ! \n";
+    else
+        std::cout << "failure ! \n";
 
     return 0;
+}
+
+
+int mat_mult(std::vector<std::vector<float>> A, std::vector<std::vector<float>> B, int N)
+{
+    cudaError_t rc = cudaSuccess;
+
+    // Perform matrix multiplication C = A*B
+    // where A, B and C are NxN matrices
+    int SIZE = N*N;
+
+    // Allocate memory on the host
+    std::vector<float> h_A(SIZE);
+    std::vector<float> h_B(SIZE);
+    std::vector<float> h_C(SIZE);
+
+    // Initialize matrices on the host
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            h_A[i*N+j] = A[i][j];
+            h_B[i*N+j] = B[i][j];
+        }
+    }
+
+    // Allocate memory on the device
+    float* d_A;
+    float* d_B;
+    float* d_C;
+
+    cudaMalloc(&d_A, SIZE * sizeof(float));
+    cudaMalloc(&d_B, SIZE * sizeof(float));
+    cudaMalloc(&d_C, SIZE * sizeof(float));
+
+    // Copy to device
+    rc = cudaMemcpy(d_A, &h_A[0], SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    if (rc)
+        std::cout << "error memcpy\n";
+    cudaMemcpy(d_B, &h_B[0], SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemset(d_C, 0, SIZE * sizeof(float));
+
+    // call the kernel
+    matrixMultiplication(d_A, d_B, d_C, N);
+    cudaDeviceSynchronize();
+
+    // copy memory back to host
+    cudaMemcpy(&h_C[0], d_C, SIZE * sizeof(float), cudaMemcpyDeviceToHost);
+
+    //un-flatten matrix
+    std::vector<std::vector<float>> C(N);
+
+    for (int i=0; i<N; i++) {
+        std::vector<float> c(N);
+        C[i] = c;
+        for (int j = 0; j < N; j++) {
+            C[i][j] = h_C[i * N + j];
+        }
+    }
+
+
+    // compute CPU mult
+    std::vector<std::vector<float>> C_true(N);
+    float sum;
+    for (int i=0; i<N; i++) {
+        std::vector<float> c_true(N);
+        C_true[i] = c_true;
+        for (int j = 0; j < N; j++) {
+            sum = 0.f;
+            for (int k = 0; k < N; ++k) {
+                sum += h_A[i*N+k] * h_B[k*N+j];
+            }
+            C_true[i][j] = sum;
+        }
+    }
+    bool same = true;
+    // check true
+    for (int i=0; i<N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (C[i][j] < C_true[i][j] - 0.0001 || C[i][j] > C_true[i][j] + 0.0001)
+                same = false;
+            std::cout << C[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << '\n';
+
+    for (int i=0; i<N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << C_true[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    if (same)
+        std::cout << "succes ! \n";
+    else
+        std::cout << "failure ! \n";
+
+    return 0;
+}
+
+
+
+int main() {
+    int N = 20;
+
+    std::vector<std::vector<float>> A(N);
+    std::vector<std::vector<float>> B(N);
+    for (int i = 0; i < N; i++) {
+        std::vector<float> a(N);
+        std::vector<float> b(N);
+        A[i] = a;
+        B[i] = b;
+        for (int j = 0; j < N; j++) {
+            A[i][j] = sin(i) * sin(i);
+            B[i][j] = cos(i) * cos(i);
+        }
+    }
+    mat_mult(A, B, N);
 }
