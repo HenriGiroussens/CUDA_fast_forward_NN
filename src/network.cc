f10f3928e7a630247ffa118b3210d1b168fa506e
Henri Giroussens <henri.giroussens@epita.fr>
2020-01-13 16:48:18 +0100

    We can make CNNs now

--- src/network.cc
+++ src/network.cc
@@ -7,10 +7,11 @@
 #include "layers/Activation.hh"
 #include "layers/Conv2D.hh"
 #include "layers/Flatten.hh"
+#include "layers/AveragePooling2D.hh"
 
 int main() {
 
-    std::string mod = "conv";
+    std::string mod = "small_LeNet";
 
     if (mod == "dense") {
         double input_tmp[5] = {1., 1., 1., 1., 1.};
@@ -42,7 +43,7 @@ int main() {
         }
     }
 
-    if (mod == "conv_flatten") {
+    if (mod == "conv") {
         double A_tmp[2][25] = {
                 {3., 3., 2., 1., 0.,
                         0., 0., 1., 3., 1.,
@@ -115,7 +116,7 @@ int main() {
         std::cout << '\n';
     }
 
-    if (mod == "conv") {
+    if (mod == "conv_flatten") {
         double A_tmp[2][25] = {
                 {3., 3., 2., 1., 0.,
                         0., 0., 1., 3., 1.,
@@ -197,6 +198,82 @@ int main() {
         }
         std::cout << '\n';
     }
+
+
+    if (mod == "small_LeNet") {
+        double A_tmp[2][25] = {
+                {3., 3., 2., 1., 0.,
+                        0., 0., 1., 3., 1.,
+                        3., 1., 2., 2., 3.,
+                        2., 0., 0., 2., 2,
+                        2., 0., 0., 0., 1.},
+                {3., 3., 2., 1., 0.,
+                        0., 0., 1., 3., 1.,
+                        3., 1., 2., 2., 3.,
+                        2., 0., 0., 2., 2,
+                        2., 0., 0., 0., 1.}
+        };
+        auto** A = static_cast<double **>(malloc(2 * sizeof(double *)));
+        for (int i = 0; i < 2; ++i){
+            auto* A_in = static_cast<double *>(malloc(25 * sizeof(double)));
+            A[i] = A_in;
+            for (int j = 0; j < 25; ++j)
+                A[i][j] = A_tmp[i][j];
+        }
+
+        double K_tmp [2][2][9] = {{{0., 1., 2.,
+                                           2., 2., 0.,
+                                           0., 1., 2.},
+                                          {0., 1., 2.,
+                                                  2., 2., 0.,
+                                                  0., 1., 2.}
+                                  },
+                                  {{0., 1., 2.,
+                                           2., 2., 0.,
+                                           0., 1., 2.},
+                                          {0., 1., 2.,
+                                                  2., 2., 0.,
+                                                  0., 1., 2.}
+                                  }
+        };
+        auto*** K = static_cast<double ***>(malloc(2 * sizeof(double **)));
+        for (int i = 0; i < 2; ++i){
+            auto** K1 = static_cast<double **>(malloc(2 * sizeof(double*)));
+            for (int j = 0; j < 2; ++j) {
+                auto* K2 = static_cast<double *>(malloc(25 * sizeof(double)));
+                for (int k = 0; k < 25; ++k)
+                    K2[k] = K_tmp[i][j][k];
+                K1[j] = K2;
+            }
+            K[i] = K1;
+        }
+        double W2[16] =
+                {1., 1.,
+                 -1., -1.,
+                 1., 1.,
+                 -1., -1.,
+                 1., 1.,
+                 -1., -1.,
+                 1., 1.,
+                 -1., -1.};
+        double B2[2] = {1., 1.};
+
+
+        Conv2D conv_1(5, 5, 2, K, 2, 3, "same");
+        Activation act_1("relu", 25, 2);
+        AveragePooling2D avg_1(5, 5, 2, 2, "valid");
+        Flatten flatten(4, 2);
+        Dense dense_2(8, 2, W2, B2);
+        Activation act_2("softmax", 2, 1);
+        Layer *layers[6] = {&conv_1, &act_1, &avg_1, &flatten, &dense_2, &act_2};
+        Model model(layers, 6, 5, 2);
+        double *output = model.predict(A);
+        for (int i = 0; i < 2; i++) {
+            std::cout << output[i] << ' ';
+        }
+        std::cout << '\n';
+    }
+
 
     return 0;
 }
